42b6ad9f21a0c5070c3cbdb55d23645d5aafbfd1
priya <priya.xavier@gmail.com>
2016-09-15 15:59:22 -0500

    added images

---
+++ RecipeRater/RecipeRater/RatingControl.swift
@@ -0,0 +1,25 @@
+//
+//  RatingControl.swift
+//  RecipeRater
+//
+//  Created by Priya Xavier on 9/15/16.
+//  Copyright © 2016 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+       super.init(coder: aDecoder)
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.redColor()
+        addSubview(button)
+    }
+   override func intrinsicContentSize() -> CGSize {
+    return CGSize(width: 240, height: 44)
+}   
+//
+
+}
