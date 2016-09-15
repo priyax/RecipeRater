efb95fff46bfd3c8c37d0f14c415fbc169f0cc59
priya <priya.xavier@gmail.com>
2016-09-15 10:24:26 -0500

    added buttons to VC

--- RecipeRater/RecipeRater/ViewController.swift
+++ RecipeRater/RecipeRater/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameMealLabel: UILabel!
+    
+    // MARK: Actions
+    
+    @IBAction func setDefaultLabel(sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
